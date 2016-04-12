a416f50fbd234a3f30bf4ac843174659a52d957b
Sundström <Magnus.Sundstrom@flir.se>
2016-04-12 16:07:52 +0200

    Testing simple webscraping with timeapi.org

---
+++ Testing webscraping/WebScrapeConsoleTest/WebScrapeConsoleTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Diagnostics;
+using System.Threading;
+
+namespace WebScrapeConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient webClient = new WebClient();
+            const string strUrl = "http://www.timeapi.org/utc/now";
+            byte[] reqHTML;
+            while (true)
+            {
+                webClient.Headers.Add("User-Agent: Other");
+                reqHTML = webClient.DownloadData(strUrl);
+                UTF8Encoding objUTF8 = new UTF8Encoding();
+                Console.WriteLine(objUTF8.GetString(reqHTML));
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
